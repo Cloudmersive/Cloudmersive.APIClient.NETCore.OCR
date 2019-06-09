d2fb3bfbb8895629043f48eeb4efa6c4e9631b8e
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-06-09 14:09:23 -0700

    Added 1.3.1

--- client/src/Cloudmersive.APIClient.NETCore.OCR.Test/Api/PreprocessingApiTests.cs
+++ client/src/Cloudmersive.APIClient.NETCore.OCR.Test/Api/PreprocessingApiTests.cs
@@ -64,6 +64,18 @@ namespace Cloudmersive.APIClient.NETCore.OCR.Test
 
         
         /// <summary>
+        /// Test PreprocessingBinarize
+        /// </summary>
+        [Test]
+        public void PreprocessingBinarizeTest()
+        {
+            // TODO uncomment below to test the method and replace null with proper value
+            //System.IO.Stream imageFile = null;
+            //var response = instance.PreprocessingBinarize(imageFile);
+            //Assert.IsInstanceOf<Object> (response, "response is Object");
+        }
+        
+        /// <summary>
         /// Test PreprocessingUnrotate
         /// </summary>
         [Test]
